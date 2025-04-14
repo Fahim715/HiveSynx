282886baee23da2d39acc9e801b4fb7db4b5f62f
Shakil Ahmed <shakilofficial0@gmail.com>
2025-04-14 23:08:29 +0600

    Update server configuration to use environment variables for host and port

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,12 @@ dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => {0
   res.send('Hello World!');
 });
 
-app.listen(5000, () => {
+app.listen(process.env.APP_PORT, process.env.APP_HOST,() => {
   connectDB();
-  console.log('Server is running on http://localhost:5000');
+  console.log(`Server is running on ${process.env.APP_HOST}:${process.env.APP_PORT}`);
 });
 
